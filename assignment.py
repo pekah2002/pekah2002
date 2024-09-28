941b65823a83476e6372dfbd99d1569012d73c37
Okemute pekah <okemutepekah@gmail.com>
2024-09-28 22:50:41 +0100

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,30 @@
+
+def main():
+    def calculate_circle_diameter(radius):
+        diameter = 2 * radius
+        return diameter
+
+    
+    def calculate_circle_circumfrence(radius):
+        ci 
+    
+    circle_radius = float(input('Enter the radius of a circle: '))
+
+    circle_diameter = calculate_circle_diameter(circle_radius)
+    print(f'The diameter is {circle_diameter}')
+
+    circle_circumfrence = calculate_circle_circumfrence(circle_radius)
+    print(f'The circumfrence is {circle_circumfrence}')
+
+    #A function for calculatin the area  of a circle
+    circle_area = calculate_circle_area(circle_radius)
+    print(f'The area is {circle_area}')rcumfrence = 2 * 3.14 * radius
+        return circumfrence
+
+    def calculate_circle_area(radius):
+        area = (3.14 * radius) **2
+        return area
+
+
+
+main()
